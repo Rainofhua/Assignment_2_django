6372c09e4d9486d0d47f8f303ed2403997bb5c7b
raino <rainofhua@hotmail.com>
2024-06-27 12:47:26 +1200

    upload to vercel

--- Assignment_2_django_project/settings.py
+++ Assignment_2_django_project/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'Assignment_2_django_project.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#  }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'verceldb',
-       'USER': 'default',
-       'PASSWORD': '6NFJRyG0IEWc',
-       'HOST': 'ep-white-grass-a7vjlt49-pooler.ap-southeast-2.aws.neon.tech',
-       'PORT': '5432',
-   }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+  }
+
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME': 'verceldb',
+#       'USER': 'default',
+#       'PASSWORD': '6NFJRyG0IEWc',
+#       'HOST': 'ep-white-grass-a7vjlt49-pooler.ap-southeast-2.aws.neon.tech',
+#       'PORT': '5432',
+#   }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
